f93a6dcec0f3603f6248bdfc2fab80b26b87a51e
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2025-01-24 00:11:14 +0530

    updated functions typing

--- typescript/functions.ts
+++ typescript/functions.ts
@@ -8,3 +8,20 @@ function addition (num1:number,num2:number,num3?:number):number{
 }
 console.log(addition(1,2))
 console.log(addition(1,2,3))
+
+function nameMerging(first : string,last : string,middle ? : string){
+    if(middle){
+        return first + " " + middle + " " + last
+    }
+    return first + " " + last
+}
+function callName ( func : (f:string,l:string,m?:string) => string,param1:string,param2:string,param3?:string):void{
+    if(param3){
+        console.log(`hello `,func(param1,param2,param3))
+    }
+    else{
+        console.log(`hello `,func(param1,param2))
+    }
+}
+callName(nameMerging,"TEJA","SRI","RAMYA")
+callName(nameMerging,"TEJA","RAMYA")
