57a0227704c7fbc20d453a95c655a50b0fdef757
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2025-01-24 21:39:19 +0530

    added about primitive data types

---
+++ javascript/datatypes/primitives.js
@@ -0,0 +1,23 @@
+//number 
+const num = 1123
+const newnum = 1123.03
+let numnew = BigInt(1111111122233311)
+//string 
+const name = "ramyasri"
+const newname = 'Ramuuu sri'
+//boolean
+const isABaby = true
+//null
+const nullvalue = null
+//undefined 
+const undefinedvalue = undefined
+console.log(typeof null) //object
+
+//javascript types are dynamic 
+const x = 1
+x = "sky"
+x = true
+
+
+const s = "ramya" + "sri" + 11 + 23
+console.log(s)
