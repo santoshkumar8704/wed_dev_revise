6cb0b30f24fff60a9efe8bd9d278a449ce8cf68f
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2025-01-24 11:34:00 +0530

    modified classes by adding getter and setter to oop.ts

--- typescript/oop.ts
+++ typescript/oop.ts
@@ -1,11 +1,18 @@
 class Persons {
     protected nam : string
+    private age:number = 3
     constructor (nam : string){
         this.nam = nam
         this.greet()
     }
     private greet (){
         console.log(this.nam,"  says hello.....")
+    }
+    getage () {
+        console.log(this.age)
+    }
+    setage (newage:number) {
+        this.age = newage
     }
 }
 class Human extends Persons {
@@ -14,5 +21,9 @@ class Human extends Persons {
     }
 }
 const person1 = new Persons("Ramya")
+person1.getage()
+person1.setage(4)
+person1.getage()
 const human1 = new Human("santosh")
 human1.callMe()
+
