9776e44bc39c541bcf5447bb964e92e2aa1254fd
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2025-01-24 21:51:33 +0530

    added about reference types to js

---
+++ javascript/datatypes/reference.js
@@ -0,0 +1,22 @@
+//array
+const numarr = [1,2,3]
+const mixarr = [1,2,"ramya",true] //arrays can be homogenous
+
+//object
+const obj1 = {
+    name:"ramyasri",
+    dob: 112303,
+    printname: function () {
+        console.log(this.name)
+    }
+}
+
+//functions
+const a = function () {
+    console.log("a")
+}
+//fat arrow functions(es6)
+
+const ab = () => {
+    console.log("ab")
+}
