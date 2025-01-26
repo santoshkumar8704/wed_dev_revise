333d013f25660dcddc52db3d96b12f9560bfdb64
YERUBANDI SATYA SANTOSH KUMAR <132978331+santoshkumar8704@users.noreply.github.com>
2025-01-27 00:14:22 +0530

    added navbar

---
+++ next/routing/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    
+      <nav className='flex justify-around mx-auto'>
+        <h1>Home</h1>
+        <div className='flex'>
+            <h1>Performance</h1>
+            <h1>Reliablity</h1>
+        </div>
+      </nav>
+  )
+}
+
+export default NavBar
